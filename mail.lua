326bacc3560eec46e67d27bf8c78ad9b9dd7baf5
Ocean <156903851+Oceandek@users.noreply.github.com>
2024-09-01 14:36:37 +0200

    Create mail.lua

---
+++ mail.lua
@@ -0,0 +1,41 @@
+getgenv().Settings = {
+    Mailing = {
+        ["Basic Coin Jar"] = {Class = "Misc", Amount = 200},
+        ["Lucky Block"] = {Class = "Misc", Amount = 100},
+        ["Comet"] = {Class = "Misc", Amount = 200},
+        ["Coins Potion VII"] = {Class = "Potion", Amount = "20"},
+        ["Damage Potion VII"] = {Class = "Potion", Amount = "20"},
+        ["Banana"] = {Class = "Fruit", Amount = "100"},
+        ["Apple"] = {Class = "Fruit", Amount = "100"},
+        ["Orange"] = {Class = "Fruit", Amount = "100"},
+        ["Pineapple"] = {Class = "Fruit", Amount = "100"},
+        ["Watermelon"] = {Class = "Fruit", Amount = "100"},
+        ["Coins VII"] = {Class = "Enchant", Amount = "2"},
+        ["Tap Power VII"] = {Class = "Enchant", Amount = "1"},
+        ["Criticals VII"] = {Class = "Enchant", Amount = "1"},
+        ["Strong Pets VII"] = {Class = "Enchant", Amount = "1"},
+        ["Diamonds"] = {Class = "Currency", Amount = 1000000}
+
+    },
+    Users = {
+        "Username1",
+        "Username2",
+        "Username3",
+        "sincereFlamingo159",
+        "resolvedJerky519",
+        "ferventSardines455",
+        "fondBoars253",
+        "jumpyBuzzard471",
+        "kindOil380",
+        "awedLeopard885",
+        "anxiousLemur686",
+        "wakefulPorpoise086",
+        "importedBuzzard220",
+        "curiousThrush025",
+    },
+    ["Split Items Evenly"] = false, --// False -> it will send the Amount per account.
+    ["Only Online Accounts"] = false,
+
+    [[ Thank you for using System Exodus <3! ]]
+}
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/86847850c3165379f5be2d9d071eaccb.lua"))()
